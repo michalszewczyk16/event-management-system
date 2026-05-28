4e38b2ff70eca8e33d9606ba2fa78248adb62dbb
michalszewczyk16 <138154370+michalszewczyk16@users.noreply.github.com>
2026-05-28 11:18:59 +0200

    Aktualizacja pom.xml pod JDK 26
    Updated User class to include username field and constructors.

--- src/main/java/com/example/demo/model/entity/User.java
+++ src/main/java/com/example/demo/model/entity/User.java
@@ -1,15 +1,43 @@
-package com.projekt.eventsystem.model.entity;
+package com.example.demo.model.entity;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Data // To generuje gettery i settery automatycznie
-@NoArgsConstructor // Pusty konstruktor
-@AllArgsConstructor // Konstruktor ze wszystkimi polami
 public class User {
     private Long id;
-    private String firstName;
-    private String lastName;
+    private String username;
     private String email;
+
+    // Konstruktor bezargumentowy
+    public User() {
+    }
+
+    // Konstruktor pełny
+    public User(Long id, String username, String email) {
+        this.id = id;
+        this.username = username;
+        this.email = email;
+    }
+
+    // Gettery i Settery
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
